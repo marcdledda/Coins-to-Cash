8d90f62bae947facb3faaca9a970476b46bc590d
marcdledda <marcdl636@gmail.com>
2018-01-31 09:37:49 -0600

    finished exercise

--- coinsToCash.js
+++ coinsToCash.js
@@ -1,1 +1,10 @@
 console.log("JS TEST");
+
+let piggyBank = {pennies: 100, nickels: 20, dimes: 10, quarters: 4};
+let pennyDoll = (piggyBank.pennies / 100);
+let nickelDoll = (piggyBank.nickels / 20);
+let dimeDoll = (piggyBank.dimes / 10);
+let quarterDoll = (piggyBank.quarters / 4);
+let dollarAmount = (pennyDoll + nickelDoll + dimeDoll + quarterDoll);
+
+console.log("Dollar Amount = $", dollarAmount);
